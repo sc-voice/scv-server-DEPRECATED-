a231d42e102d09628a6261a7299a1b72f463befe
Karl Lew <karl@firepick.org>
2022-07-01 07:24:41 -0700

    v1.37.2 update-latest

--- scripts/sc-voice.mjs
+++ scripts/sc-voice.mjs
@@ -11,15 +11,21 @@ import {
 // ensure argv is actually for script instead of mocha
 var argv = process.argv[1].match(__filename) && process.argv || [];
 argv.filter(a => a==='--log-debug').length && (logger.level = 'debug');
-var port = argv.reduce((a,v)=>(v==='-3000' ? 3000 : a), undefined);
+var port = argv.reduce((a,v)=>{
+  if (v === '-3000') { 
+    return 3000;    // legacy option
+  }
+  if (v.startsWith('--port:')) {
+    return Number(v.replace('--port:', ''));
+  }
+  return a;
+}, undefined);
 var apiUrl = argv.some((a) => a === '--staging')
     ? 'http://staging.suttacentral.net/api'
     : 'http://suttacentral.net/api';
 
-logger.info("sc-voice: starting");
 let protocol = argv.some((a) => a === '--ssl') ? "https" : "http";
 let opts = { apiUrl, protocol };
 port != null && (opts.port = port);
 let scv = new ScvServer(opts);
 await scv.initialize();
-logger.info("sc-voice: server is running");
