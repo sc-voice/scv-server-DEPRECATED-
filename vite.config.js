79ee081990bb01b4cf612638fe4ef1cd62d76bde
Karl Lew <karl@firepick.org>
2022-07-17 16:05:47 -0700

    v1.47.0 github pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/scv-server/',
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
