b086c068b0b5919ddc7fa01df0e5d2e4b9a922dc
Karl Lew <karl@firepick.org>
2022-07-23 07:37:16 -0700

    v1.53.0 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 DIR=`dirname $0`
 SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
-
 APP=$DIR/..
-
 set -e
 
 echo "$SCRIPT: git subtree pull --prefix dist origin gh-pages"
