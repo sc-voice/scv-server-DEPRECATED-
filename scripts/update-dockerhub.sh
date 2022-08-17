5efc9cc35797efd27f8a8914c0789b440b726023
Karl Lew <karl@firepick.org>
2022-08-17 06:00:23 -0700

    v1.104.1 += update-dockerhub

---
+++ scripts/update-dockerhub.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+DIR=`dirname $0`; 
+pushd $DIR/..
+SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+echo -e "${SCRIPT}: BEGIN `date`"
+
+# get token to be able to talk to Docker Hub
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DKR_USER}'", "password": "'${DKR_PWD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+REPO=scv-server
+AGO="8 hours ago"
+
+
+IMAGE_TAGS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DKR_USER}/${REPO}/tags/?page_size=10000 | jq -r '.results|.[]|.name')
+
+echo -e "$SCRIPT: scanning DockerHub ${DKR_USER}/${REPO}"
+for TAG in ${IMAGE_TAGS}
+do
+  IMAGE="${DKR_USER}/${REPO}:${TAG}"
+  UPDATED=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DKR_USER}/${REPO}/tags/${TAG}/?page_size=10000 | jq -r '.last_updated')
+  UPDATEDSECS=$(date --date "$UPDATED" +'%s')
+  AGOSECS=$(date --date "$AGO" +'%s')
+  if [ $UPDATEDSECS -gt $AGOSECS ]; then
+    echo -e "$SCRIPT: keeping current image $IMAGE ${UPDATEDSECS}"
+  else 
+    echo -e "$SCRIPT: deleting old image $IMAGE ${UPDATEDSECS}"
+    curl -s  -X DELETE  -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DKR_USER}/${REPO}/tags/${TAG}/
+  fi
+done
+
+npm run build:docker
+docker login -u $DKR_USER -p $DKR_PWD
+docker image push -a scvoice/scv-server
+
+echo -e "${SCRIPT}: END `date`"
