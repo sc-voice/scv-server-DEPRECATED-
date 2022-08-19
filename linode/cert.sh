794eb6ca16e99466379c7761471b3078696f87d2
Karl Lew <karl@firepick.org>
2022-08-19 06:49:34 -0700

    v1.113.0 += certbot

---
+++ linode/cert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DIR=`dirname $0`; 
+pushd $DIR/..
+SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+echo -e "${SCRIPT}: BEGIN `date`"
+
+if type certbot; then
+  echo $SCRIPT: certbot is installed
+else
+  echo $SCRIPT: installing certbot
+  sudo apt-get install certbot
+fi
+
+EMAIL=$1
+if [ "$EMAIL" == "" ]; then
+  echo $SCRIPT: expected email address
+  exit 1
+fi
+
+CERTDIR=./local/certbot
+mkdir -p $CERTDIR
+certbot \
+  --config-dir $CERTDIR \
+  --work-dir $CERTDIR \
+  --logs-dir $CERTDIR \
+  --agree-tos \
+  -m $EMAIL \
+  certonly --manual
+
+echo -e "${SCRIPT}: END `date`"
