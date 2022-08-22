feefcba8d2eb7cfbc613deaef5c6dc809339dc39
Karl Lew <karl@firepick.org>
2022-08-22 13:55:35 -0700

    v bullseye

--- linode/bullseye/02-configure-nginx.sh
+++ linode/bullseye/02-configure-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=`dirname $0`
 SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
-APPDIR=$DIR/..
+APPDIR=$DIR/../..
 
 echo $SCRIPT: BEGIN `date`
 NGINX_FILE=nginx-scv-server.conf
