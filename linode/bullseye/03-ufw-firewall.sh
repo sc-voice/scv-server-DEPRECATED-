97cd7017c0ac788320b85889acde0c083877b5ba
Karl Lew <karl@firepick.org>
2022-08-24 06:34:16 -0700

    v1.120.0 += linode

---
+++ linode/bullseye/03-ufw-firewall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DIR=`dirname $0`
+SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+
+set -e
+
+echo $SCRIPT: BEGIN `date`
+
+echo $SCRIPT: allow port 80 HTTP
+sudo ufw allow proto tcp from any to any port 80
+echo $SCRIPT: allow port 443 SSL
+sudo ufw allow proto tcp from any to any port 443
+echo $SCRIPT: allow port 8000 HTTP for NGINX verification
+sudo ufw allow proto tcp from any to any port 8000
+
+echo $SCRIPT: Firewall status
+sudo ufw status verbose 
+
+echo $SCRIPT: END `date`
