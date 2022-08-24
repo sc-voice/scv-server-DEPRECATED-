57917d2305440ac7c10870ceceec28c2728126bd
Karl Lew <karl@firepick.org>
2022-08-24 06:56:28 -0700

    v1.123.0 += ufw

--- linode/bullseye/03-ufw-firewall.sh
+++ linode/bullseye/03-ufw-firewall.sh
@@ -7,21 +7,21 @@ set -e
 echo $SCRIPT: BEGIN `date`
 
 echo $SCRIPT: configure port 80 HTTP
-if sudo ufw status numbered | grep 80; then
+if sudo ufw status numbered | grep -e " 80/"; then
   echo $SCRIPT: firewall rule conflict with existing rules
   exit 1
 fi
 sudo ufw allow proto tcp from any to any port 80
 
 echo $SCRIPT: configure port 443 SSL
-if sudo ufw status numbered | grep 443; then
+if sudo ufw status numbered | grep -e " 443/"; then
   echo $SCRIPT: firewall rule conflict with existing rules
   exit 1
 fi
 sudo ufw allow proto tcp from any to any port 443
 
 echo $SCRIPT: configure port 8000 HTTP for NGINX verification
-if sudo ufw status numbered | grep 8000; then
+if sudo ufw status numbered | grep -e " 8000/"; then
   echo $SCRIPT: firewall rule conflict with existing rules
   exit 1
 fi
