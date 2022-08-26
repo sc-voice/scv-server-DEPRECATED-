571d5d258393662134fb2b7a1ca10b2bf10b9315
Karl Lew <karl@firepick.org>
2022-08-26 05:43:20 -0700

    v += 06-docker.sh

--- linode/bullseye/06-docker.sh
+++ linode/bullseye/06-docker.sh
@@ -14,23 +14,15 @@ ln -s -f /var/lib/docker/volumes/$DOCKER_VOLUME/_data local
 echo -e "$SCRIPT: sudo docker compose up -d"
 sudo docker compose up -d
 
-URLPATH=/scv/play/segment/thig1.1/en/sujato/thig1.1%3A1.1/Amy
+URLPATH=scv/play/segment/thig1.1/en/sujato/thig1.1%3A1.1/Amy
 echo -e "$SCRIPT: testing localhost:8080..."
-if curl http://localhost:8080/$URLPATH; then
-  echo
-  echo -e "$SCRIPT: scv-server docker container is running (OK)"
-else 
-  echo -e "$SCRIPT: scv-server docker container is not running (ERROR)"
-  exit 1
-fi
+curl http://localhost:8080/$URLPATH;
+echo
+echo -e "$SCRIPT: scv-server docker container is running (OK)"
 
-echo -e "$SCRIPT: testing localhost (nginx reverse proxy => scv-server docker container)"
-if curl http://localhost/$URLPATH; then
-  echo
-  echo -e "$SCRIPT: reverse proxy => scv-server docker container (OK)"
-else
-  echo -e "$SCRIPT: nginx reverse proxy => scv-server docker container (ERROR)"
-  exit 1
-fi
+echo -e "$SCRIPT: testing localhost:80..."
+curl http://localhost/$URLPATH
+echo
+echo -e "$SCRIPT: reverse proxy => scv-server docker container (OK)"
 
 echo -e "${SCRIPT}: END `date`"
